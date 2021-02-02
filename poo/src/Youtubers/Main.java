ce58e80f71f42b8f41b88c8cd2a8fa49db199243
fernando-cg <72653561+fernando-cg@users.noreply.github.com>
2021-02-02 12:33:22 +0100

    pp

---
+++ poo/src/Youtubers/Main.java
@@ -0,0 +1,55 @@
+package Youtubers;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in) ;
+		
+		System.out.println("¿Cuantos youtubers quieres introducir?");
+		
+		int numero = sc.nextInt() ;
+		
+		Youtuber y[] = new Youtuber[numero] ;
+		
+		sc.nextLine() ;
+		
+		String alias , paisorigen;
+		
+		int nseg, nvid, anio ;
+		
+		for(int i = 0 ; i < y.length ; i++) {
+			
+			System.out.print("Introduzca el alias del youtuber " + (i+1) + ": ");
+			
+			alias = sc.nextLine() ;
+			
+			System.out.print("Pais de origen de " +  alias + ": ");
+			
+			paisorigen = sc.nextLine() ;
+			
+			sc.nextLine() ;
+			
+			System.out.print("Introduzca la cantidad de seguidores de " +  alias + ": ");
+			
+			nseg = sc.nextInt() ;
+			
+			System.out.print("Introduzca la cantidad de videos seguidos de " +  alias + ":");
+			
+			nvid = sc.nextInt() ;
+			
+			System.out.print("Introduzca el año de creacion del canal de " +  alias + ": ");
+			
+			anio = sc.nextInt() ;
+			
+			y[i] = new Youtuber(alias,nseg,nvid,paisorigen,anio) ;
+							
+		}
+		
+		sc.close();
+
+	}
+
+}
---
+++ poo/src/Youtubers/Youtuber.java
@@ -0,0 +1,38 @@
+package Youtubers;
+
+public class Youtuber {
+
+	
+	//atributos
+	
+	private String alias ;
+	
+	private int nseguidores ;
+	
+	private int nvideos ;
+	
+	private String paisnatal ;
+	
+	private int aniodecreacion ;
+
+	//constructor
+	
+	public Youtuber(String alias,int nseguidores, int nvideos, String paisnatal, int aniodecreacion) {
+		this.alias = alias ;
+		this.nseguidores = nseguidores;
+		this.nvideos = nvideos;
+		this.paisnatal = paisnatal;
+		this.aniodecreacion = aniodecreacion;
+	}
+
+		
+	//metodos
+	
+	//ordenar array de objetos por seguidores
+	
+	
+	
+	
+	
+	
+}
